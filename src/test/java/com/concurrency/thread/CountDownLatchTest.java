8642ea85ebffe47774b8e2e1ced3dcee93349785
WeisonWei <weixx3@126.com>
2020-04-27 11:56:18 +0800

    update

--- src/test/java/com/concurrency/thread/CountDownLatchTest.java
+++ src/test/java/com/concurrency/thread/CountDownLatchTest.java
@@ -15,6 +15,16 @@ import java.util.concurrent.Executors;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CountDownLatchTest {
 
+    /**
+     * CountDownLatch -- > 计数器闭锁
+     * CountDownLatch是通过一个计数器来实现的，计数器的初始化值为线程的数量。
+     * 每当一个线程完成了自己的任务后，计数器的值就相应得减1。当计数器到达0时，表示所有的线程都已完成任务，然后在闭锁上等待的线程就可以恢复执行任务。
+     * 链接：https://www.jianshu.com/p/4b6fbdf5a08f
+     * 链接：https://www.jianshu.com/p/bb5105303d85
+     *
+     * @throws InterruptedException
+     */
+
     @Test
     public void countDownLatchTest() throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(20);
@@ -34,14 +44,14 @@ public class CountDownLatchTest {
 
     @Test
     public void noCountDownLatchTest() throws InterruptedException {
-        log.info(Thread.currentThread().getName() + "主线程开始执行==>" );
+        log.info(Thread.currentThread().getName() + "主线程开始执行==>");
         ExecutorService executorPool = Executors.newFixedThreadPool(3);
 
         for (int i = 0; i < 20; i++) {
             executorPool.submit(() -> {
-                log.info(Thread.currentThread().getName() + "====>" );
+                log.info(Thread.currentThread().getName() + "====>");
             });
         }
-        log.info(Thread.currentThread().getName() + "主线程执行结束!==>" );
+        log.info(Thread.currentThread().getName() + "主线程执行结束!==>");
     }
 }
--- src/test/java/com/concurrency/thread/SemaphoreTest.java
+++ src/test/java/com/concurrency/thread/SemaphoreTest.java
@@ -15,6 +15,19 @@ import java.util.concurrent.Executors;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SemaphoreTest {
 
+    /**
+     * Semaphore --> 类似阀门的功能
+     * Semaphore也叫信号量，在JDK1.5被引入，可以用来控制同时访问特定资源的线程数量，通过协调各个线程，以保证合理的使用资源。
+     * Semaphore内部维护了一组虚拟的许可，许可的数量可以通过构造函数的参数指定。
+     * 访问特定资源前，必须使用acquire方法获得许可，如果许可数量为0，该线程则一直阻塞，直到有可用许可。
+     * 访问资源后，使用release释放许可。
+     * Semaphore和ReentrantLock类似，获取许可有公平策略和非公平许可策略，默认情况下使用非公平策略。
+     * 链接：https://www.jianshu.com/p/0090341c6b80
+     * Semaphore可以用来做流量分流，特别是对公共资源有限的场景，比如数据库连接。
+     *
+     * @throws InterruptedException
+     */
+
     @Test
     public void semaphoreAddTest() throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(10);
