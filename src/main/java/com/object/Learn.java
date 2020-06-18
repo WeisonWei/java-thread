805e42c7c6035596429380dabaca201e5b4412e6
weixiaoxing <weixiaoxing@meishubao.com>
2020-06-18 19:36:15 +0800

    add jol-cli

---
+++ src/main/java/com/object/Learn.java
@@ -0,0 +1,9 @@
+package com.object;
+
+public class Learn {
+    private static final String name = "Weison";
+
+    public static void main(String[] args) {
+        System.out.println("--name--{}");
+    }
+}
