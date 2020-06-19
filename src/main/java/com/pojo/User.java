afd99ecfbb3167b36e3b9a01149c3e44cd8f0e33
weixiaoxing <weixiaoxing@meishubao.com>
2020-06-19 22:03:07 +0800

    add atmoic test

---
+++ src/main/java/com/pojo/User.java
@@ -0,0 +1,16 @@
+package com.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String name;
+
+    public Integer age;
+
+}
