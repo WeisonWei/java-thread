acfe3264f7829231989df7aa93d2f0bd5d5e60e3
weisonwei <weixx3@126.com>
2020-06-23 00:23:27 +0800

    update ReadWriteLockTest

--- src/test/java/com/thread/concurrency/lock/ReadWriteLockTest.java
+++ src/test/java/com/thread/concurrency/lock/ReadWriteLockTest.java
@@ -3,6 +3,8 @@ package com.thread.concurrency.lock;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -10,10 +12,39 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class ReadWriteLockTest {
 
     @Test
-    public void readWriteLockTest() {
+    public void readWriteLockTest() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
         ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
         final Lock readLock = lock.readLock();
         final Lock writeLock = lock.writeLock();
-        int[] counts = new int[10];
+
+        new Thread(() -> {
+            try {
+                writeLock.lock();
+                TimeUnit.SECONDS.sleep(3);
+                log.info("--writeLock--");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                //不释放则read一直等待
+                writeLock.unlock();
+                countDownLatch.countDown();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                readLock.lock();
+                log.info("--readLock--");
+                countDownLatch.countDown();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                readLock.unlock();
+                countDownLatch.countDown();
+            }
+        }).start();
+
+        countDownLatch.await();
     }
 }
